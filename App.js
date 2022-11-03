0622dcd0edb009f78aba7aa41f8f3a3aa8f9df3b
Le Nguyen Phuong Thai <thailnp1303@gmail.com>
2022-11-03 23:32:51 +0700

    bài tập react native về API

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+import IconOcticon from 'react-native-vector-icons/AntDesign';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const renderItem = ({ item }) => (
+    <View style={{ backgroundColor: "orange",padding: 30}}>
+      <Text style={{ fontSize: 32 }}>Id: {item.id}</Text>
+      <Text style={{ fontSize: 20 }}>Name: {item.name}</Text>
+      <Text style={{ fontSize: 20 }}>Email: {item.email}</Text>
+      <Text style={{ fontSize: 20 }}>UserName: {item.username}</Text>
+      <Text style={{ fontSize: 20 }}>Address: {item.address.street}</Text>
     </View>
+  );
+
+  const [mydata, setData] = useState([])
+
+  useEffect(() => {
+    getData()
+
+    return () => {
+
+    }
+  }, [])
+
+  var getData = async () => {
+    var api = await axios.get('https://jsonplaceholder.typicode.com/users')
+    setData(api.data)
+  }
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={mydata}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
